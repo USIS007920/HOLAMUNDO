a703ede18be42aff536b2648b703ae25ae2f6a06
Fernando Antonio Quintanilla Soriano <usis007920@ugb.edu.sv>
2020-08-23 19:33:40 -0600

    TABLAS

---
+++ TABLAS/TABLAS/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+    Private Sub btnProcesar_Click(sender As Object, e As EventArgs) Handles btnProcesar.Click
+        Dim ntabla As SByte
+        ntabla = txtntabla.Text
+
+        For i = 1 To 10
+            lstTabla.Items.Add(ntabla.ToString() + "x" + i.ToString() + "=" + (ntabla * i).ToString())
+        Next
+    End Sub
+End Class
