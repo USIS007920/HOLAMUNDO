7b983b86c320f3603e5e81a7b8def042eb564a3f
Fernando Antonio Quintanilla Soriano <usis007920@ugb.edu.sv>
2020-08-23 19:44:18 -0600

    TIEMPO

---
+++ TIEMPO/TIEMPO/Form1.Designer.vb
@@ -0,0 +1,196 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.cmdnuevo = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.cbxsalidad = New System.Windows.Forms.ComboBox()
+        Me.cbxentrada = New System.Windows.Forms.ComboBox()
+        Me.lblr = New System.Windows.Forms.TextBox()
+        Me.txtvalor = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(314, 99)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(204, 16)
+        Me.Label5.TabIndex = 23
+        Me.Label5.Text = "CONVERTIDOR DE TIEMPO"
+        '
+        'cmdnuevo
+        '
+        Me.cmdnuevo.BackColor = System.Drawing.Color.SlateBlue
+        Me.cmdnuevo.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmdnuevo.Location = New System.Drawing.Point(302, 329)
+        Me.cmdnuevo.Name = "cmdnuevo"
+        Me.cmdnuevo.Size = New System.Drawing.Size(90, 23)
+        Me.cmdnuevo.TabIndex = 22
+        Me.cmdnuevo.Text = "Borrador"
+        Me.cmdnuevo.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.Pink
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(439, 329)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(79, 23)
+        Me.Button2.TabIndex = 21
+        Me.Button2.Text = "Terminar"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.CadetBlue
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(367, 288)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(100, 23)
+        Me.Button1.TabIndex = 20
+        Me.Button1.Text = "Convertir"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'cbxsalidad
+        '
+        Me.cbxsalidad.BackColor = System.Drawing.Color.IndianRed
+        Me.cbxsalidad.FormattingEnabled = True
+        Me.cbxsalidad.Items.AddRange(New Object() {"Milisegundo", "Segundo", "Minuto", "Hora", "Dia", "Semana", "Mes", "Año", "Decada", "Siglo"})
+        Me.cbxsalidad.Location = New System.Drawing.Point(439, 184)
+        Me.cbxsalidad.Name = "cbxsalidad"
+        Me.cbxsalidad.Size = New System.Drawing.Size(121, 21)
+        Me.cbxsalidad.TabIndex = 19
+        '
+        'cbxentrada
+        '
+        Me.cbxentrada.BackColor = System.Drawing.Color.SandyBrown
+        Me.cbxentrada.FormattingEnabled = True
+        Me.cbxentrada.Items.AddRange(New Object() {"Milisegundo", "Segundo", "Minuto", "Hora", "Dia", "Semana", "Mes", "Año", "Decada", "Siglo"})
+        Me.cbxentrada.Location = New System.Drawing.Point(271, 184)
+        Me.cbxentrada.Name = "cbxentrada"
+        Me.cbxentrada.Size = New System.Drawing.Size(121, 21)
+        Me.cbxentrada.TabIndex = 18
+        '
+        'lblr
+        '
+        Me.lblr.BackColor = System.Drawing.Color.DarkSeaGreen
+        Me.lblr.Location = New System.Drawing.Point(351, 249)
+        Me.lblr.Name = "lblr"
+        Me.lblr.Size = New System.Drawing.Size(122, 20)
+        Me.lblr.TabIndex = 17
+        '
+        'txtvalor
+        '
+        Me.txtvalor.BackColor = System.Drawing.Color.DarkGray
+        Me.txtvalor.Location = New System.Drawing.Point(367, 158)
+        Me.txtvalor.Name = "txtvalor"
+        Me.txtvalor.Size = New System.Drawing.Size(100, 20)
+        Me.txtvalor.TabIndex = 16
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(375, 223)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(76, 15)
+        Me.Label4.TabIndex = 15
+        Me.Label4.Text = "Resultado:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(416, 187)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(19, 15)
+        Me.Label3.TabIndex = 14
+        Me.Label3.Text = "A:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(241, 187)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(29, 15)
+        Me.Label2.TabIndex = 13
+        Me.Label2.Text = "De:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.DarkTurquoise
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(364, 127)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(103, 16)
+        Me.Label1.TabIndex = 12
+        Me.Label1.Text = "Ingrese valor:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.DeepSkyBlue
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.cmdnuevo)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.cbxsalidad)
+        Me.Controls.Add(Me.cbxentrada)
+        Me.Controls.Add(Me.lblr)
+        Me.Controls.Add(Me.txtvalor)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label5 As Label
+    Friend WithEvents cmdnuevo As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents cbxsalidad As ComboBox
+    Friend WithEvents cbxentrada As ComboBox
+    Friend WithEvents lblr As TextBox
+    Friend WithEvents txtvalor As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+End Class
---
+++ TIEMPO/TIEMPO/Form1.vb
@@ -0,0 +1,232 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If cbxentrada.Text = "Milisegundo" And cbxsalidad.Text = "Milisegundo" Then
+            lblr.Text = Val(txtvalor.Text)
+        ElseIf cbxentrada.Text = "Milisegundo" And cbxsalidad.Text = "Segundo" Then
+            lblr.Text = Val(txtvalor.Text) / 1000
+        ElseIf cbxentrada.Text = "Milisegundo" And cbxsalidad.Text = "Minuto" Then
+            lblr.Text = Val(txtvalor.Text) / 60000
+        ElseIf cbxentrada.Text = "Milisegundo" And cbxsalidad.Text = "Hora" Then
+            lblr.Text = Val(txtvalor.Text) / 3600000.0
+        ElseIf cbxentrada.Text = "Milisegundo" And cbxsalidad.Text = "Dia" Then
+            lblr.Text = Val(txtvalor.Text) / 86400000.0
+        ElseIf cbxentrada.Text = "Milisegundo" And cbxsalidad.Text = "Semana" Then
+            lblr.Text = Val(txtvalor.Text) / 604800000.0
+        ElseIf cbxentrada.Text = "Milisegundo" And cbxsalidad.Text = "Mes" Then
+            lblr.Text = Val(txtvalor.Text) / 2628000000.0
+        ElseIf cbxentrada.Text = "Milisegundo" And cbxsalidad.Text = "Año" Then
+            lblr.Text = Val(txtvalor.Text) / 31540000000.0
+        ElseIf cbxentrada.Text = "Milisegundo" And cbxsalidad.Text = "Decada" Then
+            lblr.Text = Val(txtvalor.Text) / 315400000000.0
+        ElseIf cbxentrada.Text = "Milisegundo" And cbxsalidad.Text = "Siglo" Then
+            lblr.Text = Val(txtvalor.Text) / 3154000000000.0
+        End If
+
+        If cbxentrada.Text = "Segundo" And cbxsalidad.Text = "Sugundo" Then
+            lblr.Text = Val(txtvalor.Text)
+        ElseIf cbxentrada.Text = "Segundo" And cbxsalidad.Text = "Milisegundo" Then
+            lblr.Text = Val(txtvalor.Text) * 1000
+        ElseIf cbxentrada.Text = "Segundo" And cbxsalidad.Text = "Minuto" Then
+            lblr.Text = Val(txtvalor.Text) / 60
+        ElseIf cbxentrada.Text = "Segundo" And cbxsalidad.Text = "Hora" Then
+            lblr.Text = Val(txtvalor.Text) / 3600
+        ElseIf cbxentrada.Text = "Segundo" And cbxsalidad.Text = "Dia" Then
+            lblr.Text = Val(txtvalor.Text) / 86400
+        ElseIf cbxentrada.Text = "Segundo" And cbxsalidad.Text = "Semana" Then
+            lblr.Text = Val(txtvalor.Text) / 604800000.0
+        ElseIf cbxentrada.Text = "Segundo" And cbxsalidad.Text = "Mes" Then
+            lblr.Text = Val(txtvalor.Text) / 2628000000.0
+        ElseIf cbxentrada.Text = "Segundo" And cbxsalidad.Text = "Año" Then
+            lblr.Text = Val(txtvalor.Text) / 31540000000.0
+        ElseIf cbxentrada.Text = "Segundo" And cbxsalidad.Text = "Decada" Then
+            lblr.Text = Val(txtvalor.Text) / 315400000000.0
+        ElseIf cbxentrada.Text = "Segundo" And cbxsalidad.Text = "Siglo" Then
+            lblr.Text = Val(txtvalor.Text) / 3154000000000.0
+        End If
+
+        If cbxentrada.Text = "Minuto" And cbxsalidad.Text = "Minuto" Then
+            lblr.Text = Val(txtvalor.Text)
+        ElseIf cbxentrada.Text = "Minuto" And cbxsalidad.Text = "Milisegundo" Then
+            lblr.Text = Val(txtvalor.Text) * 60000
+        ElseIf cbxentrada.Text = "Minuto" And cbxsalidad.Text = "Segundo" Then
+            lblr.Text = Val(txtvalor.Text) * 60
+        ElseIf cbxentrada.Text = "Minuto" And cbxsalidad.Text = "Hora" Then
+            lblr.Text = Val(txtvalor.Text) / 60
+        ElseIf cbxentrada.Text = "Minuto" And cbxsalidad.Text = "Dia" Then
+            lblr.Text = Val(txtvalor.Text) / 1440
+        ElseIf cbxentrada.Text = "Minuto" And cbxsalidad.Text = "Semana" Then
+            lblr.Text = Val(txtvalor.Text) / 10080
+        ElseIf cbxentrada.Text = "Minuto" And cbxsalidad.Text = "Mes" Then
+            lblr.Text = Val(txtvalor.Text) / 438000
+        ElseIf cbxentrada.Text = "Minuto" And cbxsalidad.Text = "Año" Then
+            lblr.Text = Val(txtvalor.Text) / 525600
+        ElseIf cbxentrada.Text = "Minuto" And cbxsalidad.Text = "Decada" Then
+            lblr.Text = Val(txtvalor.Text) / 5256000.0
+        ElseIf cbxentrada.Text = "Minuto" And cbxsalidad.Text = "Siglo" Then
+            lblr.Text = Val(txtvalor.Text) / 52560000.0
+        End If
+
+        If cbxentrada.Text = "Hora" And cbxsalidad.Text = "Hora" Then
+            lblr.Text = Val(txtvalor.Text)
+        ElseIf cbxentrada.Text = "Hora" And cbxsalidad.Text = "Milisegundo" Then
+            lblr.Text = Val(txtvalor.Text) * 3600000.0
+        ElseIf cbxentrada.Text = "Hora" And cbxsalidad.Text = "Segundo" Then
+            lblr.Text = Val(txtvalor.Text) * 3600
+        ElseIf cbxentrada.Text = "Hora" And cbxsalidad.Text = "Minuto" Then
+            lblr.Text = Val(txtvalor.Text) * 60
+        ElseIf cbxentrada.Text = "Hora" And cbxsalidad.Text = "Dia" Then
+            lblr.Text = Val(txtvalor.Text) / 24
+        ElseIf cbxentrada.Text = "Hora" And cbxsalidad.Text = "Semana" Then
+            lblr.Text = Val(txtvalor.Text) / 168
+        ElseIf cbxentrada.Text = "Hora" And cbxsalidad.Text = "Mes" Then
+            lblr.Text = Val(txtvalor.Text) / 730
+        ElseIf cbxentrada.Text = "Hora" And cbxsalidad.Text = "Año" Then
+            lblr.Text = Val(txtvalor.Text) / 8760
+        ElseIf cbxentrada.Text = "Hora" And cbxsalidad.Text = "Decada" Then
+            lblr.Text = Val(txtvalor.Text) / 87600
+        ElseIf cbxentrada.Text = "Hora" And cbxsalidad.Text = "Siglo" Then
+            lblr.Text = Val(txtvalor.Text) / 876000
+        End If
+
+        If cbxentrada.Text = "Dia" And cbxsalidad.Text = "Dia" Then
+            lblr.Text = Val(txtvalor.Text)
+        ElseIf cbxentrada.Text = "Dia" And cbxsalidad.Text = "Milisegundo" Then
+            lblr.Text = Val(txtvalor.Text) * 86400000.0
+        ElseIf cbxentrada.Text = "Dia" And cbxsalidad.Text = "Segundo" Then
+            lblr.Text = Val(txtvalor.Text) * 86400
+        ElseIf cbxentrada.Text = "Dia" And cbxsalidad.Text = "Minuto" Then
+            lblr.Text = Val(txtvalor.Text) * 1440
+        ElseIf cbxentrada.Text = "Dia" And cbxsalidad.Text = "Hora" Then
+            lblr.Text = Val(txtvalor.Text) * 24
+        ElseIf cbxentrada.Text = "Dia" And cbxsalidad.Text = "Semana" Then
+            lblr.Text = Val(txtvalor.Text) / 7
+        ElseIf cbxentrada.Text = "Dia" And cbxsalidad.Text = "Mes" Then
+            lblr.Text = Val(txtvalor.Text) / 30.417
+        ElseIf cbxentrada.Text = "Dia" And cbxsalidad.Text = "Año" Then
+            lblr.Text = Val(txtvalor.Text) / 365
+        ElseIf cbxentrada.Text = "Dia" And cbxsalidad.Text = "Decada" Then
+            lblr.Text = Val(txtvalor.Text) / 3650
+        ElseIf cbxentrada.Text = "Dia" And cbxsalidad.Text = "Siglo" Then
+            lblr.Text = Val(txtvalor.Text) / 36500
+        End If
+
+        If cbxentrada.Text = "Semana" And cbxsalidad.Text = "Semana" Then
+            lblr.Text = Val(txtvalor.Text)
+        ElseIf cbxentrada.Text = "Semana" And cbxsalidad.Text = "Milisegundo" Then
+            lblr.Text = Val(txtvalor.Text) * 604800000.0
+        ElseIf cbxentrada.Text = "Semana" And cbxsalidad.Text = "Segundo" Then
+            lblr.Text = Val(txtvalor.Text) * 604800
+        ElseIf cbxentrada.Text = "Semana" And cbxsalidad.Text = "Minuto" Then
+            lblr.Text = Val(txtvalor.Text) * 10080
+        ElseIf cbxentrada.Text = "Semana" And cbxsalidad.Text = "Hora" Then
+            lblr.Text = Val(txtvalor.Text) * 168
+        ElseIf cbxentrada.Text = "Semana" And cbxsalidad.Text = "Dia" Then
+            lblr.Text = Val(txtvalor.Text) * 7
+        ElseIf cbxentrada.Text = "Semana" And cbxsalidad.Text = "Mes" Then
+            lblr.Text = Val(txtvalor.Text) / 4.345
+        ElseIf cbxentrada.Text = "Semana" And cbxsalidad.Text = "Año" Then
+            lblr.Text = Val(txtvalor.Text) / 52.143
+        ElseIf cbxentrada.Text = "Semana" And cbxsalidad.Text = "Decada" Then
+            lblr.Text = Val(txtvalor.Text) / 521
+        ElseIf cbxentrada.Text = "Semana" And cbxsalidad.Text = "Siglo" Then
+            lblr.Text = Val(txtvalor.Text) / 5214
+        End If
+
+        If cbxentrada.Text = "Mes" And cbxsalidad.Text = "Mes" Then
+            lblr.Text = Val(txtvalor.Text)
+        ElseIf cbxentrada.Text = "Mes" And cbxsalidad.Text = "Milisegundo" Then
+            lblr.Text = Val(txtvalor.Text) * 2628000000.0
+        ElseIf cbxentrada.Text = "Mes" And cbxsalidad.Text = "Segundo" Then
+            lblr.Text = Val(txtvalor.Text) * 628000000.0
+        ElseIf cbxentrada.Text = "Mes" And cbxsalidad.Text = "Minuto" Then
+            lblr.Text = Val(txtvalor.Text) * 43800
+        ElseIf cbxentrada.Text = "Mes" And cbxsalidad.Text = "Hora" Then
+            lblr.Text = Val(txtvalor.Text) * 730
+        ElseIf cbxentrada.Text = "Mes" And cbxsalidad.Text = "Dia" Then
+            lblr.Text = Val(txtvalor.Text) * 30.417
+        ElseIf cbxentrada.Text = "Mes" And cbxsalidad.Text = "Semana" Then
+            lblr.Text = Val(txtvalor.Text) * 4.345
+        ElseIf cbxentrada.Text = "Mes" And cbxsalidad.Text = "Año" Then
+            lblr.Text = Val(txtvalor.Text) / 12
+        ElseIf cbxentrada.Text = "Mes" And cbxsalidad.Text = "Decada" Then
+            lblr.Text = Val(txtvalor.Text) / 120
+        ElseIf cbxentrada.Text = "Mes" And cbxsalidad.Text = "Siglo" Then
+            lblr.Text = Val(txtvalor.Text) / 1200
+        End If
+
+        If cbxentrada.Text = "Año" And cbxsalidad.Text = "Año" Then
+            lblr.Text = Val(txtvalor.Text)
+        ElseIf cbxentrada.Text = "Año" And cbxsalidad.Text = "Milisegundo" Then
+            lblr.Text = Val(txtvalor.Text) * 31540000000.0
+        ElseIf cbxentrada.Text = "Año" And cbxsalidad.Text = "Segundo" Then
+            lblr.Text = Val(txtvalor.Text) * 31540000.0
+        ElseIf cbxentrada.Text = "Año" And cbxsalidad.Text = "Minuto" Then
+            lblr.Text = Val(txtvalor.Text) * 525600
+        ElseIf cbxentrada.Text = "Año" And cbxsalidad.Text = "Hora" Then
+            lblr.Text = Val(txtvalor.Text) * 8760
+        ElseIf cbxentrada.Text = "Año" And cbxsalidad.Text = "Dia" Then
+            lblr.Text = Val(txtvalor.Text) * 365
+        ElseIf cbxentrada.Text = "Año" And cbxsalidad.Text = "Semana" Then
+            lblr.Text = Val(txtvalor.Text) * 52.143
+        ElseIf cbxentrada.Text = "Año" And cbxsalidad.Text = "Decada" Then
+            lblr.Text = Val(txtvalor.Text) / 10
+        ElseIf cbxentrada.Text = "Año" And cbxsalidad.Text = "Siglo" Then
+            lblr.Text = Val(txtvalor.Text) / 100
+        ElseIf cbxentrada.Text = "Año" And cbxsalidad.Text = "Mes" Then
+            lblr.Text = Val(txtvalor.Text) * 12
+        End If
+
+        If cbxentrada.Text = "Decada" And cbxsalidad.Text = "Decada" Then
+            lblr.Text = Val(txtvalor.Text)
+        ElseIf cbxentrada.Text = "Decada" And cbxsalidad.Text = "Mes" Then
+            lblr.Text = Val(txtvalor.Text) * 120
+        ElseIf cbxentrada.Text = "Decada" And cbxsalidad.Text = "Milisegundo" Then
+            lblr.Text = Val(txtvalor.Text) * 315400000000.0
+        ElseIf cbxentrada.Text = "Decada" And cbxsalidad.Text = "Minuto" Then
+            lblr.Text = Val(txtvalor.Text) * 5256000.0
+        ElseIf cbxentrada.Text = "Decada" And cbxsalidad.Text = "Segundo" Then
+            lblr.Text = Val(txtvalor.Text) * 315400000.0
+        ElseIf cbxentrada.Text = "Decada" And cbxsalidad.Text = "Hora" Then
+            lblr.Text = Val(txtvalor.Text) * 87600
+        ElseIf cbxentrada.Text = "Decada" And cbxsalidad.Text = "Dia" Then
+            lblr.Text = Val(txtvalor.Text) * 3650
+        ElseIf cbxentrada.Text = "Decada" And cbxsalidad.Text = "Semana" Then
+            lblr.Text = Val(txtvalor.Text) * 521
+        ElseIf cbxentrada.Text = "Decada" And cbxsalidad.Text = "Siglo" Then
+            lblr.Text = Val(txtvalor.Text) / 10
+        ElseIf cbxentrada.Text = "Decada" And cbxsalidad.Text = "Año" Then
+            lblr.Text = Val(txtvalor.Text) * 10
+        End If
+
+        If cbxentrada.Text = "Siglo" And cbxsalidad.Text = "Siglo" Then
+            lblr.Text = Val(txtvalor.Text)
+        ElseIf cbxentrada.Text = "Siglo" And cbxsalidad.Text = "Mes" Then
+            lblr.Text = Val(txtvalor.Text) * 1200
+        ElseIf cbxentrada.Text = "Siglo" And cbxsalidad.Text = "Milisegundo" Then
+            lblr.Text = Val(txtvalor.Text) * 3154000000000.0
+        ElseIf cbxentrada.Text = "Siglo" And cbxsalidad.Text = "Segundo" Then
+            lblr.Text = Val(txtvalor.Text) * 3154000000.0
+        ElseIf cbxentrada.Text = "Siglo" And cbxsalidad.Text = "Minuto" Then
+            lblr.Text = Val(txtvalor.Text) * 52560000.0
+        ElseIf cbxentrada.Text = "Siglo" And cbxsalidad.Text = "Dia" Then
+            lblr.Text = Val(txtvalor.Text) * 36500
+        ElseIf cbxentrada.Text = "Siglo" And cbxsalidad.Text = "Decada" Then
+            lblr.Text = Val(txtvalor.Text) * 10
+        ElseIf cbxentrada.Text = "Siglo" And cbxsalidad.Text = "Semana" Then
+            lblr.Text = Val(txtvalor.Text) * 5214
+        ElseIf cbxentrada.Text = "Siglo" And cbxsalidad.Text = "Hora" Then
+            lblr.Text = Val(txtvalor.Text) * 876000
+        ElseIf cbxentrada.Text = "Siglo" And cbxsalidad.Text = "Año" Then
+            lblr.Text = Val(txtvalor.Text) * 100
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        End
+    End Sub
+
+    Private Sub cmdnuevo_Click(sender As Object, e As EventArgs) Handles cmdnuevo.Click
+        lblr.Text = ""
+        txtvalor.Text = ""
+    End Sub
+End Class
