05ff5221ff1f3d8e84511ef423f10e43e0a44e1c
Fernando Antonio Quintanilla Soriano <usis007920@ugb.edu.sv>
2020-08-30 16:04:09 -0600

    CAJERO

---
+++ CAJERO/CAJERO/Form1.Designer.vb
@@ -0,0 +1,120 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnProcesar = New System.Windows.Forms.Button()
+        Me.lstEjercicios = New System.Windows.Forms.ListBox()
+        Me.btnParImpar = New System.Windows.Forms.Button()
+        Me.txtnum = New System.Windows.Forms.TextBox()
+        Me.lblnum = New System.Windows.Forms.Label()
+        Me.btnPrimo = New System.Windows.Forms.Button()
+        Me.btnCajero = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnProcesar
+        '
+        Me.btnProcesar.Location = New System.Drawing.Point(427, 330)
+        Me.btnProcesar.Name = "btnProcesar"
+        Me.btnProcesar.Size = New System.Drawing.Size(96, 45)
+        Me.btnProcesar.TabIndex = 0
+        Me.btnProcesar.Text = "Mostrar Pares e impares"
+        Me.btnProcesar.UseVisualStyleBackColor = True
+        '
+        'lstEjercicios
+        '
+        Me.lstEjercicios.FormattingEnabled = True
+        Me.lstEjercicios.Location = New System.Drawing.Point(402, 73)
+        Me.lstEjercicios.Name = "lstEjercicios"
+        Me.lstEjercicios.Size = New System.Drawing.Size(146, 238)
+        Me.lstEjercicios.TabIndex = 1
+        '
+        'btnParImpar
+        '
+        Me.btnParImpar.Location = New System.Drawing.Point(22, 154)
+        Me.btnParImpar.Name = "btnParImpar"
+        Me.btnParImpar.Size = New System.Drawing.Size(85, 56)
+        Me.btnParImpar.TabIndex = 2
+        Me.btnParImpar.Text = "Par o Impar"
+        Me.btnParImpar.UseVisualStyleBackColor = True
+        '
+        'txtnum
+        '
+        Me.txtnum.Location = New System.Drawing.Point(100, 89)
+        Me.txtnum.Name = "txtnum"
+        Me.txtnum.Size = New System.Drawing.Size(100, 20)
+        Me.txtnum.TabIndex = 3
+        '
+        'lblnum
+        '
+        Me.lblnum.AutoSize = True
+        Me.lblnum.Location = New System.Drawing.Point(47, 92)
+        Me.lblnum.Name = "lblnum"
+        Me.lblnum.Size = New System.Drawing.Size(47, 13)
+        Me.lblnum.TabIndex = 4
+        Me.lblnum.Text = "Numero:"
+        '
+        'btnPrimo
+        '
+        Me.btnPrimo.Location = New System.Drawing.Point(131, 154)
+        Me.btnPrimo.Name = "btnPrimo"
+        Me.btnPrimo.Size = New System.Drawing.Size(80, 56)
+        Me.btnPrimo.TabIndex = 5
+        Me.btnPrimo.Text = "Primo?"
+        Me.btnPrimo.UseVisualStyleBackColor = True
+        '
+        'btnCajero
+        '
+        Me.btnCajero.Location = New System.Drawing.Point(264, 162)
+        Me.btnCajero.Name = "btnCajero"
+        Me.btnCajero.Size = New System.Drawing.Size(108, 48)
+        Me.btnCajero.TabIndex = 6
+        Me.btnCajero.Text = "Cajero"
+        Me.btnCajero.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnCajero)
+        Me.Controls.Add(Me.btnPrimo)
+        Me.Controls.Add(Me.lblnum)
+        Me.Controls.Add(Me.txtnum)
+        Me.Controls.Add(Me.btnParImpar)
+        Me.Controls.Add(Me.lstEjercicios)
+        Me.Controls.Add(Me.btnProcesar)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnProcesar As Button
+    Friend WithEvents lstEjercicios As ListBox
+    Friend WithEvents btnParImpar As Button
+    Friend WithEvents txtnum As TextBox
+    Friend WithEvents lblnum As Label
+    Friend WithEvents btnPrimo As Button
+    Friend WithEvents btnCajero As Button
+End Class
---
+++ CAJERO/CAJERO/Form1.vb
@@ -0,0 +1,63 @@
+﻿Public Class Form1
+    Private Sub btnProcesar_Click(sender As Object, e As EventArgs) Handles btnProcesar.Click
+        lstEjercicios.Items.Clear()
+
+        lstEjercicios.Items.Add("Numero pares del 1 al 20")
+        For i = 2 To 100 Step 2
+            lstEjercicios.Items.Add(i)
+        Next
+
+        lstEjercicios.Items.Add("Numero impares del 1 al 20")
+        For i = 1 To 100 Step 2
+            lstEjercicios.Items.Add(i)
+        Next
+    End Sub
+
+    Private Sub btnParImpar_Click(sender As Object, e As EventArgs) Handles btnParImpar.Click
+        Dim num = txtnum.Text
+        If num Mod 2 = 0 Then
+            MessageBox.Show("El numero: " + num + ", es par")
+        Else
+            MessageBox.Show("El numero: " + ", es impar")
+        End If
+    End Sub
+
+    Private Sub btnPrimo_Click(sender As Object, e As EventArgs) Handles btnPrimo.Click
+        Dim num = txtnum.Text
+        Dim i = 0, acu = 1
+        While i < 3 And acu <= num
+            If num Mod acu = 0 Then
+                i += 1
+            End If
+            acu += 1
+        End While
+        If i <= 2 Then
+            MessageBox.Show("Es primo")
+        Else
+            MessageBox.Show("No es primo")
+        End If
+    End Sub
+
+    Private Sub btnCajero_Click(sender As Object, e As EventArgs) Handles btnCajero.Click
+        Dim denominaciones() = {100, 50, 20, 10, 5, 1, 0.5, 0.25, 0.1, 0.05, 0.01}, valor = txtnum.Text, cantidad = 0
+        Dim denomina As String
+
+        lstEjercicios.Items.Clear()
+
+        For Each denominacion In denominaciones
+            While valor > 0 And denominacion <= valor
+                valor = Math.Round(valor - denominacion, 2)
+                cantidad += 1
+            End While
+            If cantidad > 0 Then
+                If denominacion <= 1 Then
+                    denomina = " moneda de"
+                Else
+                    denomina = " billete de"
+                End If
+                lstEjercicios.Items.Add(cantidad.ToString() + denomina + denominacion.ToString())
+                cantidad = 0
+            End If
+        Next
+    End Sub
+End Class
